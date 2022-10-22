7fbe675487fb9ed8436f7b7161e013a3eb7f24ff
Siddharth Singh Yadav <ssingh@iiitmanipur.ac.in>
2022-10-22 22:57:59 +0530

    'first'

---
+++ learning_users/basic_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+# SET THE NAMESPACE!
+app_name = 'basic_app'
+
+# Be careful setting the name to just /login use userlogin instead!
+urlpatterns=[
+    path('register/',views.register,name='register'),
+]
